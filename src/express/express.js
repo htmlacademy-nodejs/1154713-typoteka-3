8b9482c75e8c53e39e8d5f8f74099bded807eac6
Iurii_Tugolukov <yurj9999@yandex.ru>
2022-02-28 08:52:15 +0300

    start task

--- src/express/express.js
+++ src/express/express.js
@@ -1,5 +1,6 @@
 'use strict';
 
+const {resolve} = require(`path`);
 const express = require(`express`);
 const {MY, ARTICLES} = require(`./consts`);
 
@@ -8,6 +9,11 @@ const articlesRoutes = require(`./routes/articles/articlesRoutes`);
 
 const app = express();
 
+app.set(`view engine`, `pug`);
+app.set(`views`, resolve(__dirname,`templates`));
+
+app.use(express.static(resolve(__dirname, `public`)));
+
 app.use(MY, myRoutes);
 app.use(ARTICLES, articlesRoutes);
 
@@ -15,5 +21,7 @@ app.get(`/`, (_, res) => res.send(`/`));
 app.get(`/register`, (_, res) => res.send(`/register`));
 app.get(`/login`, (_, res) => res.send(`/login`));
 app.get(`/search`, (_, res) => res.send(`/search`));
+
+//app.get(`/404`, (_, res) => res.render(`errors\\404.pug`));
 
 app.listen(8080, () => console.log(`Front server started`));
