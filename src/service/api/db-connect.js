edcfedc40c116fd10a07691de03ec66a2ece22ff
Yurj Tugolukov <yurj9999@yandex.ru>
2022-05-23 17:44:38 +0300

    fixes

--- src/service/api/db-connect.js
+++ src/service/api/db-connect.js
@@ -3,14 +3,16 @@
 const Sequelize = require(`sequelize`);
 const {DB_NAME, DB_USER, DB_PASSWORD, DB_HOST, DB_PORT} = process.env;
 
-module.exports = new Sequelize(DB_NAME, DB_USER, DB_PASSWORD, {
-  host: DB_HOST,
-  port: DB_PORT,
-  dialect: `postgres`,
-  pool: {
-    max: 5,
-    min: 0,
-    acquire: 10000,
-    idle: 10000,
-  },
-});
+module.exports = {
+  createDBInstance: () => new Sequelize(DB_NAME, DB_USER, DB_PASSWORD, {
+    host: DB_HOST,
+    port: DB_PORT,
+    dialect: `postgres`,
+    pool: {
+      max: 5,
+      min: 0,
+      acquire: 10000,
+      idle: 10000,
+    },
+  }),
+};
--- src/service/cli/server.js
+++ src/service/cli/server.js
@@ -7,15 +7,18 @@ const {getServerConfig} = require(`./server-config`);
 
 const {getLogger} = require(`../lib/logger`);
 
-const sequelize = require(`../api/db-connect`);
+const {createDBInstance} = require(`../api/db-connect`);
 
 module.exports = {
   run: async (port = 3000) => {
     const logger = getLogger(`server`);
 
+    const db = createDBInstance();
+
     try {
       logger.info(`Trying to connect to database...`);
-      await sequelize.authenticate();
+
+      await db.authenticate();
       logger.info(`Connection to DB is OK`);
 
       const fileData = await readFile(`mock.json`);
